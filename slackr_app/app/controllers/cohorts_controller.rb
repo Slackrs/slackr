08b3c79328af6c75f4341843a6ef6be0a508a414
Kelly Stevens <kllystvns@gmail.com>
2015-06-24 15:15:42 -0400

    routes and controller files

--- slackr_app/app/controllers/cohorts_controller.rb
+++ slackr_app/app/controllers/cohorts_controller.rb
@@ -18,5 +18,12 @@ class CohortsController < ApplicationController
 		end
 	end
 
+	# def troubled
+	# 	@cohort = Cohort.find(params[:id])
+	# 	@troublestudents = @cohort.students.
+
+	# 	#pass in 
+	# end
+
 
 end
