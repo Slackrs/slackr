c4bd352f7514d573935a87e2a54bbd251764291a
Ben Sayeg <bensayeg@gmail.com>
2015-06-26 16:13:07 -0400

    pulling heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
   port:                 587,
   domain:               'gmail.com',
   user_name:            'slackr.melville',
-  password:             'hermanmelville',
+  password:             secrets.gmail_password,
   authentication:       'plain',
   enable_starttls_auto: true  }
 
