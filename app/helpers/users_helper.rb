4364979b1106d6bed158d97aef6be3b19bbdf6ba
Ryan Burke <ryaneburke@gmail.com>
2015-06-27 15:10:11 -0400

    working gravatar default

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,7 @@
 module UsersHelper
 	def gravatar_url(email, size)
-	  gravatar = Digest::MD5::hexdigest(email).downcase
-	  url = "http://gravatar.com/avatar/#{gravatar}.png?s=#{size}"
+	  gravatar_id = Digest::MD5::hexdigest(email).downcase
+	  default_url = "http://i.dailymail.co.uk/i/pix/2011/06/10/article-2001697-0C816FEB00000578-38_306x423.jpg"
+	  url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{CGI::escape(default_url)}"
 	end
 end
